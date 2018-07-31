971a3d41492664a55f3e53be655d2fc1e1bd153b
sakunyo <sakuya.s@gmail.com>
2018-07-31 19:32:58 +0900

    WIP set-up React w/TS

--- app/javascript/packs/hello.ts
+++ app/javascript/packs/hello.ts
@@ -1,4 +1,7 @@
 // Run this example by adding <%= javascript_pack_tag 'hello_typescript' %> to the head of your layout file,
 // like app/views/layouts/application.html.erb.
 
+import "../src/hello.scss";
+import "../src/index.tsx";
+
 console.log('Hello world from typescript');
